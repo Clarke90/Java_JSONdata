05be63ab321a7f6f042c011e2989ea814c3474b9
ChadClark_Dev <chadclarke@bell.net>
2016-03-17 21:35:09 -0400

    test scroll

--- Scripts/projects.js
+++ Scripts/projects.js
@@ -36,27 +36,5 @@
     request.send();
 
 
-// jquery smooth scroller from id to id 
-    $(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-});
-
-
-
-
-
-
-
 })();
 
